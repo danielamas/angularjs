69ad9457cb78182108de5141b05392e418376d6b
Daniel Lamas <danielamas@gmail.com>
2018-02-02 17:00:58 -0200

    class 2 and 3

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,6 +1,13 @@
-angular.module('alurapic').controller('FotosController', function ($scope) {
-    $scope.foto = {
-        titulo : 'Leão',
-        url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    };
+angular.module('alurapic').controller('FotosController', function ($scope, $http) {
+    
+    $scope.fotos = [];
+
+    $http.get('/v1/fotos')
+    .success(function(retorno) {
+        $scope.fotos = retorno;
+    })
+    .error(function(erro) {
+        console.log(erro)
+    }); 
+
 });
